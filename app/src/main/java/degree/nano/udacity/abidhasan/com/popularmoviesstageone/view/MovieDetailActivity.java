0cebf7d213ecb660044bd77832f3e397928e5f37
Abid Hasan <la4508@gmail.com>
2017-03-05 17:03:21 +0600

    movie detail view init commit

---
+++ app/src/main/java/degree/nano/udacity/abidhasan/com/popularmoviesstageone/view/MovieDetailActivity.java
@@ -0,0 +1,15 @@
+package degree.nano.udacity.abidhasan.com.popularmoviesstageone.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import degree.nano.udacity.abidhasan.com.popularmoviesstageone.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
